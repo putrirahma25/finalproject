46c7fa2fe8ce4dbccf56960d8e93d2fe545345af
putrirahma25 <megaponsel8493053@gmail.com>
2024-12-09 21:13:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,11 @@ app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static', 'uploads')
 
 # Database connection
 db = connector.connect(
-   user="fpcc", password="{your_password}", host="fpwebcc.mysql.database.azure.com", port=3306, database="{your_database}
+   user="fpcc", 
+    password="{your_password}", 
+    host="fpwebcc.mysql.database.azure.com", 
+    port=3306, 
+    database="{your_database}
 db_config = {
     'host': 'localhost',
     'user': 'root',
